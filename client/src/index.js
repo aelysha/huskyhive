70d06abd2165f17fbbaa4e9b0cbb4ad00a16f426
Aelysha <aelysha@uw.edu>
2025-05-09 05:25:12 -0700

    Final landing page and misc changes

--- client/src/index.js
+++ client/src/index.js
@@ -6,12 +6,13 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId="854157406658-r39p4ihqe52uju7fu8bo2at3lu8j552m.apps.googleusercontent.com">
       <App />
-    </GoogleOAuthProvider>;
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
