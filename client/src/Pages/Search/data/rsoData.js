f03c3e9411cee1bfeb1cd744d246cd36e0494a30
Aelysha <aelysha@uw.edu>
2025-05-29 01:18:08 -0700

    Hardcoded imagery/data to supplement search temporarily

---
+++ client/src/Pages/Search/data/rsoData.js
@@ -0,0 +1,152 @@
+export const rsoData = [
+  {
+    "rso_name": "The Garden of Ideas",
+    "description": "The Garden of Ideas is the UW's undergraduate philosophy journal. The purpose of The Garden of Ideas is to democratize philosophy and broaden philosophical discussion by publishing a variety of academic and non-academic work. We publish two issues of the journal each year and receive and accept submissions from a wide range of academic disciplines.",
+    "interest_tags": "[\"philosophy\",\"academic\",\"journalism\",\"discussion\",\"interdisciplinary\"]",
+    "social_media": "[{\"Instagram\":\"https://www.instagram.com/gardenofideasuw/\"},{\"Website\":\"https://www.gardenofideasuw.com/\"}]",
+    "search_bio": "Publishes diverse undergrad philosophy from multiple disciplines.",
+    "rso_type": "General",
+    "rso_logo": "/logos/garden-logo.jpg",
+    "rso_background": "/backdrops/garden-logo.jpg"
+  },
+  {
+    "rso_name": "Husky Math Club",
+    "description": "\"To bring together UW undergraduate mathematics majors and undergraduates interested in mathematics together to socialize, exchange ideas, and participate in fun events. Some recent events we have held are: an Integration Bee/Calculus Bee, Husky Math Talks, which are lectures in mathematics and related areas given by UW undergraduates, a fundraising event through MOD Pizza, online study breaks during COVID, an internship information session, and board game nights. We are planning other events such as advanced mathematics reading groups, and panels about industry and research opportunities.\"",
+    "interest_tags": "[\"math\",\"STEM\",\"networking\"]",
+    "social_media": "[{\"Facebook\":\"https://www.facebook.com/groups/1285639468478723\"},{\"Discord\":\"https://discord.gg/Hzmnn4QTjC\"}]",
+    "search_bio": "Brings together math enthusiasts for socializing and fun events.",
+    "rso_type": "General",
+    "rso_logo": "/logos/math-logo.jpg",
+    "rso_background": "/backdrops/math-logo.jpg"
+  },
+  {
+    "rso_name": "Women's Club Baseball",
+    "description": "\"The Women's Club Baseball team at the University of Washington is committed to creating more opportunities for women to play baseball at a higher level. We are working with the Baseball For All Foundation to create a more inclusive future of baseball. Our team is open to people of all experience levels -- whether you have experience in baseball or softball, or none at all. We practice once a week on Sundays and compete yearly in a tournament through Baseball for All. \"",
+    "interest_tags": "[\"baseball\",\"athletic\"]",
+    "social_media": "[{\"Instagram\":\"https://www.instagram.com/uwwomensclubbaseball/\"}]",
+    "search_bio": "Creates opportunities for women in baseball, open to all skill levels.",
+    "rso_type": "Sports and Rec",
+    "rso_logo": "/logos/baseball-logo.jpg",
+    "rso_background": "/backdrops/baseball-logo.jpg"
+  },
+  {
+    "rso_name": "American Society of Mechanical Engineers",
+    "description": "\"The mission of the American Society of Mechanical Engineers (ASME) at UW is to promote peer relationships and professional engineering development for students of mechanical engineering. ASME provides students with opportunities to network with the greater\r\nmechanical engineering community through social events, guest speaker lectures from industry, and tours of current projects, local businesses, and research facilities.\"",
+    "interest_tags": "[\"engineering\",\"STEM\",\"networking\"]",
+    "social_media": "[{\"Instagram\":\"https://www.instagram.com/asmeuw/#\"},{\"LinkedIn\":\"https://www.linkedin.com/company/asme-at-uw/\"},{\"Facebook\":\"https://www.facebook.com/ASMEUW/\"},{\"Discord\":\"https://discord.gg/8rSQk3bh\"}]",
+    "search_bio": "Promotes professional development for mechanical engineering students.",
+    "rso_type": "General",
+    "rso_logo": "/logos/asme-logo.jpg",
+    "rso_background": "/backdrops/asme-logo.jpg"
+  },
+  {
+    "rso_name": "The Lettuce Eating Club",
+    "description": "\"This is a group of fun-loving lettuce eaters who care about the environment. We meet once a quarter and have a fun lettuce eating event in which everyone competes to see who can eat their head of iceberg lettuce the fastest. The one who completes this challenge is crowned the King or Queen of Lettuce and shall be responsible for, with the help of the Tribunal of Lettuce Eaters, setting up the next meeting. Along with Lettuce eating, we advocate for climate justice in the community through collaborations with other RSOs.\"",
+    "interest_tags": "[\"social\",\"environmental\"]",
+    "social_media": "[{\"Website\":\"https://linktr.ee/LettuceEatingClubUW\"}]",
+    "search_bio": "Fun group of lettuce eaters advocating for climate justice through events.",
+    "rso_type": "General",
+    "rso_logo": "/logos/lettuce-logo.jpg",
+    "rso_background": "/backdrops/lettuce-logo.jpg"
+  },
+  {
+    "rso_name": "Girls Who Code - College Loops",
+    "description": "We are a college campus organization where women (and those who identify as female) with an interest in or passion for computer science and tech related fields can get together to learn, support each other, and have fun. We do not restrict membership based on major, anyone regardless of technical experience is welcome to join! We welcome those who have attended Girls Who Code Club/Immersion Programs to continue their connection with GWC in college. Although being affiliated with Girls Who Code, we do not restrict membership, everyone is welcome to join.",
+    "interest_tags": "[\"women-in-stem\",\"technology\"]",
+    "social_media": "[{\"Instagram\":\"https://www.instagram.com/uwcollegeloops/\"},{\"Email\":\"uwcollegeloops@gmail.com\"},{\"Discord\":\"https://discord.gg/KqArmTcA3r\"}]",
+    "search_bio": "Connects women and non-binary students passionate about tech.",
+    "rso_type": "General",
+    "rso_logo": "/logos/gwc-logo.jpg",
+    "rso_background": "/backdrops/gwc-logo.jpg"
+  },
+  {
+    "rso_name": "The Hiking Club at UW",
+    "description": "The Hiking Club at UW currently operates in a Discord server. Hike sign-ups, officer announcements, and club membership sign-up information and guidelines will be posted here. You must be a member to join us on trips, but signing up is easy through Discord! If you have any questions or want to see what we're all about, feel free to contact us through email (hikingclubatuw@gmail.com) or follow us on Instagram (@hikingclubatuw)! We can't wait to see you on the trails: Hike On!",
+    "interest_tags": "[\"hiking\",\"outdoor\",\"athletic\",\"trips\",\"community\"]",
+    "social_media": "[{\"Discord\":\"https://discord.com/invite/q2YJhRwxPQ\"},{\"Instagram\":\"https://www.instagram.com/hikingclubatuw/\"}]",
+    "search_bio": "Plans group hikes & outdoor events - join easily via Discord!",
+    "rso_type": "General",
+    "rso_logo": "/logos/hike-logo.jpg",
+    "rso_background": "/backdrops/hike-logo.jpg"
+  },
+  {
+    "rso_name": "Climbing Club at the University of Washington",
+    "description": "The Climbing Club is an organization focusing on rock and ice climbing, mountaineering, scrambling, hiking, skiing, snowshoeing, and any other mountain sport. If it's on a mountain, we probably do it! We focus a lot on having events where we get our members outside. All of our trips are open for all club members of various skill levels (we will let you know what kind of experience you should have prior to a specific trip so have no fear!). Once you join the club you get access to all the trips we lead as well as our free gear library. It's an amazing deal!",
+    "interest_tags": "[\"climbing\",\"bouldering\",\"outdoors\",\"mountaineering\",\"community\"]",
+    "social_media": "[{\"Discord\":\"https://discord.gg/xAXZ78rrAh\"},{\"Instagram\":\"https://www.instagram.com/climbingclubatuw/\"},{\"Website\":\"https://sites.uw.edu/climb/\"}]",
+    "search_bio": "Unites all skill levels for mountain sports and outdoor trips.",
+    "rso_type": "Sports and Rec",
+    "rso_logo": "/logos/climb-logo.jpg",
+    "rso_background": "/backdrops/climb-logo.jpg"
+  },
+  {
+    "rso_name": "Judo Club at the University of Washington",
+    "description": "Group Purpose:\n(A) Provide judo education and classes that both develop the skills and strength of experienced and inexperienced judoka alike.\n(B) Establish Judo at UW as a trusted and reputable club in the Pacific Northwest Judo Community and grow Judo @ UW\u2019s presence in the judo community.\n(C) Provide financial aid to Judo @ UW athletes who seek to compete locally or nationally",
+    "interest_tags": "[\"judo\",\"martial arts\",\"athletics\",\"community\",\"competition\"]",
+    "social_media": "[{\"Website\":\"https://judouw.notion.site/64e99921459c461caa4350a43f8c12d3?v=f77ea50eec3749e382744c0bca1bfcc8\"},{\"Instagram\":\"https://www.instagram.com/judo.uw/\"},{\"Facebook\":\"https://www.facebook.com/groups/57601299416/\"}]",
+    "search_bio": "Trains all skill levels, builds community, and supports competitors.",
+    "rso_type": "Sports and Rec",
+    "rso_logo": "/logos/judo-logo.jpg",
+    "rso_background": "/backdrops/judo-logo.jpg"
+  },
+  {
+    "rso_name": "ColorStack at the University of Washington",
+    "description": "A supportive organization created to help Black & Latinx Computer Science Students to complete their degrees and gain rewarding technical careers",
+    "interest_tags": "[\"support\",\"computer science\",\"career development\"]",
+    "social_media": "[{\"Instagram\":\"https://www.instagram.com/colorstackuw/\"},{\"LinkedIn\":\"https://www.linkedin.com/company/colorstackuw/\"},{\"Discord\":\"https://discord.gg/5hry8Ys896\"}]",
+    "search_bio": "Supports Black/Latinx CS students in earning degrees and early careers.",
+    "rso_type": "General",
+    "rso_logo": "/logos/color-logo.jpg",
+    "rso_background": "/backdrops/color-logo.jpg"
+  },
+  {
+    "rso_name": "Informatics Undergraduate Association at the University of Washington",
+    "description": "The Informatics Undergraduate Association was founded by Informatics majors for Informatics majors. It is our objective to serve the needs and interests of those in our major, but also those interested in applying to the undergraduate degree program as well.",
+    "interest_tags": "[\"informatics\",\"academic\",\"networking\",\"community\"]",
+    "social_media": "[{\"Website\":\"https://iuga.info/\"},{\"Instagram\":\"https://www.instagram.com/iuga.info/\"},{\"LinkedIn\":\"https://www.linkedin.com/company/informatics-undergraduate-association-iuga-/mycompany/\"},{\"Facebook\":\"https://www.facebook.com/groups/info.uw/\"}]",
+    "search_bio": "Supports current and future Informatics majors with resources and guidance.",
+    "rso_type": "General",
+    "rso_logo": "/logos/iuga-logo.jpg",
+    "rso_background": "/backdrops/iuga-logo.jpg"
+  },
+  {
+    "rso_name": "MESH UW",
+    "description": "Welcome to MESH, a fashion-design focused organization dedicated to cultivating creativity and style! If you're interested in fashion in any aspect--designing, admiring, styling-- you've come to the right place.\n\nAt MESH, our primary focus is on building a community that embraces members of all skill levels with sincerity and dedication. Whether you're a seasoned designer or a novice, our workshops are created to be accessible and beginner-friendly, providing everyone with an opportunity to learn and grow.These workshops will empower you with essential skills, from the fundamentals of sketching to the intricacies of sewing, enabling you to bring your unique fashion ideas to fruition with proficiency and precision.\n\nBeyond honing our members' talents, we also take immense pride in recognizing their achievements. Our annual educational EXPO held every February, is a fun social event that celebrates the our community's creativity! We bring together fashion organizations from all over Seattle to showcase their talent and provide resources for students wanting to continue fashion outside of UW!\n\nIn May, our student-led fashion show becomes the stage for emerging designers to display their visionary creations. It is a platform that not only exhibits their talents but also encourages innovation and pushes the boundaries of contemporary style.\n\nTo stay connected with the latest updates on our events and to gain exclusive insights into the world of fashion, follow us on Instagram (@mesh.uw) and join our community on Discord(https://discord.gg/ec96hxbEdn). See you at our next event!",
+    "interest_tags": "[\"fashion\",\"design\",\"runway\",\"community\",\"workshops\"]",
+    "social_media": "[{\"Instagram\":\"https://www.instagram.com/mesh.uw/\"},{\"Discord\":\"https://discord.gg/ec96hxbEdn\"},{\"LinkedIn\":\"https://www.linkedin.com/company/mesh-uw\"},{\"TikTok\":\"https://www.tiktok.com/@mesh_uw\"}]",
+    "search_bio": "Cultivates creativity in fashion design, offering workshops and events.",
+    "rso_type": "General",
+    "rso_logo": "/logos/mesh-logo.jpg",
+    "rso_background": "/backdrops/mesh-logo.jpg"
+  },
+  {
+    "rso_name": "iQueeries",
+    "description": "iQueeries provides a safe, positive, and nurturing space for LGBTQIA+ and supportive ally students in the Information School. We work to foster a strong, supportive queer community within the iSchool as well as UW at large, and to promote positive change and social justice within the field of information sciences. iQueeries is intersectional: all identities and experiences are welcome and encouraged to share their voices.\n",
+    "interest_tags": "[\"lgbtq+\",\"community\",\"informatics\",\"social justice\",\"support\"]",
+    "social_media": "[{\"Instagram\":\"https://www.instagram.com/iQueeries/\"},{\"Discord\":\"https://discord.com/invite/yVFrBNSR5V\"},{\"Facebook\":\"https://www.facebook.com/groups/iqueeries/?mibextid=oMANbw\"}]",
+    "search_bio": "Supports LGBTQIA+ students and allies in info science, fostering community.",
+    "rso_type": "General",
+    "rso_logo": "/logos/queer-logo.jpg",
+    "rso_background": "/backdrops/queer-logo.jpg"
+  },
+  {
+    "rso_name": "DubsTech",
+    "description": "\"We at DubsTech aim to help students grow their tech skills. We organize a variety of open-to-all workshops, competitions, talks, and more. We've had guest speakers from various tech companies such as Google, Amazon, Microsoft, Facebook, and more. We\u2019re also known for holding UW\u2019s first data science hackathon, the Datathon, and the only UX competition on campus, the Protothon! Here, participants test their rapid prototyping and design thinking skills in two industry inspired, time-sensitive design challenges, and help design for the future. Irrespective of your major, background, or skill level, everyone is welcome!\"",
+    "interest_tags": "[\"STEM\",\"technology\",\"social\",\"engineering\"]",
+    "social_media": "[{\"Website\":\"http://dubstech.info/\"},{\"Instagram\":\"https://www.instagram.com/dubstechuw/#\"},{\"LinkedIn\":\"https://www.linkedin.com/company/dubstech/\"},{\"Facebook\":\"https://www.facebook.com/DubsTech\"},{\"YouTube\":\"https://www.youtube.com/@dubstech/videos\"},{\"Discord\":\"https://discord.gg/weHm3Sg8Z5\"}]",
+    "search_bio": "Helps students grow tech skills through workshops, competitions, and talks.",
+    "rso_type": "General",
+    "rso_logo": "/logos/dubst-logo.jpg",
+    "rso_background": "/backdrops/dubst-logo.jpg"
+  },
+  {
+    "rso_name": "Phi Sigma Rho",
+    "description": "\"Phi Sigma Rho is a social sorority centered around forging a strong community for women, genderqueer, and non-binary students pursuing engineering. Our emphasis is around the relationships that we form and the memories that we make, but as engineers, we also support each other in our academic and professional endeavors. As a national organization, members of Phi Rho are part of a nationwide network of current members and alumni that thrives even after graduation.\n\nFollow our instagram @uwphisigmarho to stay up to date on recruitment in the fall and other events year round!\"",
+    "interest_tags": "[\"STEM\",\"engineering\",\"community\",\"professional support\"]",
+    "social_media": "[{\"Website\":\"https://www.uwphisigmarho.com/\",\"Facebook\":\"https://www.facebook.com/uwphisigmarho/\",\"Instagram\":\"https://www.instagram.com/uwphisigmarho/?hl=en\"}]",
+    "search_bio": "Building community for women, genderqueer, and non-binary engineers.",
+    "rso_type": "Greek Life",
+    "rso_logo": "/logos/psr-logo.jpg",
+    "rso_background": "/backdrops/psr-logo.jpg"
+  }
+];
